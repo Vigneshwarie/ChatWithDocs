f1d0dc818cb9127fe74359831fe69e49a8f406ae
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-06-24 18:55:37 -0700

    Added functionality to chunk the document and store them into vector database

---
+++ app.py
@@ -0,0 +1,74 @@
+import os
+from unstructured.partition.pdf import partition_pdf
+from unstructured.staging.base import convert_to_dict
+from sentence_transformers import SentenceTransformer
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+load_dotenv()
+CONNECTION_STRING = os.getenv("MONGODB_CONNECTION_STRING")
+DB_NAME = os.getenv("MONGODB_DB_NAME")
+COLLECTION_NAME = os.getenv("MONGODB_COLLECTION_NAME")
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+uri = CONNECTION_STRING
+db_name = DB_NAME
+collection_name = COLLECTION_NAME
+
+elements = partition_pdf("documents/Document2.pdf",
+                         strategy="hi_res",
+                         infer_table_structured=True)
+
+records = convert_to_dict(elements)
+
+model = SentenceTransformer('microsoft/mpnet-base')
+
+for record in records:
+    txt = record['text']
+    record['embedding'] = model.encode(txt).tolist()
+
+# Connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# delete all first
+client[db_name][collection_name].delete_many({})
+
+# insert
+client[db_name][collection_name].insert_many(records)
+query = "What is about software engineering and agile methodlogy?"
+
+vector_query = model.encode(query).tolist()
+print("The vector query==", vector_query)
+print("\n")
+
+pipeline = [
+    {
+        "$search": {
+            "index": "default",
+            "knnBeta": {
+                "vector": vector_query,
+                "path": "embedding",
+                "k": 5,
+            }
+        }
+    },
+    {
+        "$project": {
+            "embedding": 0,
+            "_id": 0,
+            "score": {
+                "$meta": "searchScore"
+            },
+        }
+    }
+]
+
+results = list(client[db_name][collection_name].aggregate(pipeline))
